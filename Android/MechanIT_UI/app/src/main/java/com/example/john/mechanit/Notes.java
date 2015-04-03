3356f64597ae2dfd5bd37d805e60aec44b68042b
john-jaffe <adrenix12@gmail.com>
2015-04-02 22:13:09 -0400

    added functional notes activity

--- Android/MechanIT_UI/app/src/main/java/com/example/john/mechanit/Notes.java
+++ Android/MechanIT_UI/app/src/main/java/com/example/john/mechanit/Notes.java
@@ -1,36 +1,66 @@
 package com.example.john.mechanit;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.john.mechanit.data.NoteItem;
+import com.example.john.mechanit.data.NotesDataSource;
 
-public class Notes extends ActionBarActivity {
+import java.util.List;
+
+
+public class Notes extends ListActivity {
+    private static final int EDITOR_ACTIVITY_REQUEST = 1001;
+    private static final int MENU_DELETE_ID = 1002;
+    private int currentNoteId;
+    private NotesDataSource datasource;
+    List<NoteItem> notesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        registerForContextMenu(getListView());
+
+        datasource = new NotesDataSource(this);
+
+        refreshDisplay();
+
         findViewById(R.id.button30).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Notes.this.startActivity(new Intent(Notes.this, NewNote.class));
+                createNote();
             }
         });
         // Navigation drawer
-        final String[] values = getResources().getStringArray(R.array.nav_drawer_items);
-        ((ListView) findViewById(R.id.navList4)).setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, values));
-        NavigationDrawerSetup nds = new NavigationDrawerSetup((ListView) findViewById(R.id.navList4),
-                (DrawerLayout) findViewById(R.id.drawer_layout), values, getSupportActionBar(), this);
-        nds.configureDrawer();
+        //final String[] values = getResources().getStringArray(R.array.nav_drawer_items);
+        //((ListView) findViewById(R.id.navList4)).setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, values));
+        //NavigationDrawerSetup nds = new NavigationDrawerSetup((ListView) findViewById(R.id.navList4),
+        //        (DrawerLayout) findViewById(R.id.drawer_layout), values, getSupportActionBar(), this);
+        //nds.configureDrawer();
     }
+    private void createNote() {
+        NoteItem note = NoteItem.getNew();
+        Intent intent = new Intent(this, NewNote.class);
+        intent.putExtra("key", note.getKey());
+        intent.putExtra("text", note.getText());
+        startActivityForResult(intent, EDITOR_ACTIVITY_REQUEST);
+    }
+    private void refreshDisplay() {
+        notesList = datasource.findAll();
+        ArrayAdapter<NoteItem> adapter =
+                new ArrayAdapter<NoteItem>(this, R.layout.list_item_layout, notesList);
+        setListAdapter(adapter);
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -53,4 +83,46 @@ public class Notes extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+
+        NoteItem note = notesList.get(position);
+        Intent intent = new Intent(this, NewNote.class);
+        intent.putExtra("key", note.getKey());
+        intent.putExtra("text", note.getText());
+        startActivityForResult(intent, EDITOR_ACTIVITY_REQUEST);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == EDITOR_ACTIVITY_REQUEST && resultCode == RESULT_OK) {
+            NoteItem note = new NoteItem();
+            note.setKey(data.getStringExtra("key"));
+            note.setText(data.getStringExtra("text"));
+            datasource.update(note);
+            refreshDisplay();
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
+        currentNoteId = (int)info.id;
+        menu.add(0,MENU_DELETE_ID, 0, "Delete");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        if (item.getItemId() == MENU_DELETE_ID) {
+            NoteItem note = notesList.get(currentNoteId);
+            datasource.remove(note);
+            refreshDisplay();
+        }
+
+        return super.onContextItemSelected(item);
+    }
 }
+
+
